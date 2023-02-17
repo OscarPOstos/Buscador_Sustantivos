20c2f4ff3d348bb09c150112c19ec0541aa9718c
oscarpostos <theholderhaizaki@gmail.com>
2023-02-17 21:45:26 +0100

    Colocando sistema de indexacion

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if uploaded_file is not None:
 
     # To read file as string:
     string_data = stringio.read()
-    indexes = set_indexes(string_data.split("\n\n"))
+    indexes = set_indexes(string_data.split("\n"))
     st.write(string_data)
 
     with st.spinner('Buscando los sustantivos 😎'):
