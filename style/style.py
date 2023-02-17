d08b6df899690281c7476d6716a6e5f13e4044a1
oscarpostos <theholderhaizaki@gmail.com>
2023-02-18 00:15:15 +0100

    Retocando CSS

--- style/style.py
+++ style/style.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+
 def set_style():
-    st.markdown("<style>strong {font-size: 20px;}</style>", unsafe_allow_html=True)
+    st.markdown("""<style>
+    section.main.css-k1vhr4.egzxvld5 {
+                    background-image: url("https://static.vecteezy.com/system/resources/previews/001/734/547/large_2x/seamless-pattern-of-open-books-vector.jpg");
+                    background-size: cover;
+                }
+    
+    strong {font-size: 20px;}
+    </style>""", unsafe_allow_html=True)
